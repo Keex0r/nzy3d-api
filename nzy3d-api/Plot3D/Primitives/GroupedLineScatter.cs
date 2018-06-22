1817e184b9442b7923f7f51f1a7437dd0af378f5
Jens Wallauer <Jens.Wallauer@gmail.com>
2018-06-22 09:03:21 +0200

    GroupedLineScatter: Added LineWidth, handling of z-Index, and improved comments

--- nzy3d-api/Plot3D/Primitives/GroupedLineScatter.cs
+++ nzy3d-api/Plot3D/Primitives/GroupedLineScatter.cs
@@ -23,27 +23,48 @@ namespace nzy3D.Plot3D.Primitives
         {
             _bbox = new BoundingBox3d();
             PointWidth = 1;
+            LineWidth = 1;
             Color = Color.BLACK;
         }
 
+        /// <summary>
+        /// Initializes a new GroupedLineScatter object
+        /// </summary>
+        /// <param name="coordinates">A list of coordinate arrays. Each list item is rendered as a separate series in a different color.</param>
         public GroupedLineScatter(List<Coord3d[]> coordinates) :
                 this(coordinates, Color.BLACK)
         {
         }
 
-        public GroupedLineScatter(List<Coord3d[]> coordinates, Color rgb, float width = 1)
+        /// <summary>
+        /// Initializes a new GroupedLineScatter object
+        /// </summary>
+        /// <param name="coordinates">A list of coordinate arrays. Each list item is rendered as a separate series in a different color.</param>
+        /// <param name="rgb">The default color. It is recommended to use the <see cref="GroupedLineScatter.GroupedLineScatter(List{Coord3d[]}, Color[], float, float)"/> constructor and a provide a list of colors.</param>
+        /// <param name="pointwidth">The size of the scatter markers</param>
+        /// <param name="linewidth">The width of the line</param>
+        public GroupedLineScatter(List<Coord3d[]> coordinates, Color rgb, float pointwidth = 8, float linewidth = 2)
         {
             _bbox = new BoundingBox3d();
             Data = coordinates;
-            PointWidth = width;
+            this.PointWidth = pointwidth;
+            this.LineWidth = linewidth;
             Color = rgb;
         }
 
-        public GroupedLineScatter(List<Coord3d[]> coordinates, Color[] colors, float width = 1)
+        /// <summary>
+        /// Initializes a new GroupedLineScatter object
+        /// </summary>
+        /// <param name="coordinates">A list of coordinate arrays. Each list item is rendered as a separate series in a different color.</param>
+        /// <param name="colors">A list of colors. The series are colored in order of this list with rollover if more series than colors are provided</param>
+        /// <param name="pointwidth">The size of the scatter markers</param>
+        /// <param name="linewidth">The width of the line</param>
+        public GroupedLineScatter(List<Coord3d[]> coordinates, Color[] colors, float pointwidth = 8, float linewidth = 2)
         {
             _bbox = new BoundingBox3d();
             Data = coordinates;
-            PointWidth = width;
+            this.PointWidth = pointwidth;
+            this.LineWidth = linewidth;
             Colors = colors;
         }
 
@@ -58,14 +79,18 @@ namespace nzy3D.Plot3D.Primitives
 
             _transform?.Execute();
 
-            
             if (_coordinates != null)
             {
                 int k = 0;
+                GL.PointSize(_pointwidth);
+                GL.LineWidth(LineWidth);
+                //Enable in order to provide correct z-indexing of the series
+                GL.Enable(EnableCap.DepthTest);
+                GL.DepthFunc(DepthFunction.Less);
+                //Render each set of coordinates
                 foreach (Coord3d[] c in _coordinates)
                 {
-                    GL.PointSize(_pointwidth);
-                    GL.Begin(BeginMode.Points);
+                    //Setup colors
                     if (_colors == null)
                     {
                         GL.Color4(_color.r, _color.g, _color.b, _color.a);
@@ -76,24 +101,28 @@ namespace nzy3D.Plot3D.Primitives
                         k++;
                         if (k >= _colors.Length) k = 0; //Roll over the color list
                     }
-                    //Draw all points and lines for this series
-                    foreach(Coord3d p in c)
+                    //Draw points
+                    if (PointWidth > 0)
                     {
-                        GL.Vertex3(p.x, p.y, p.z);
+                        GL.Begin(BeginMode.Points);
+                        foreach (Coord3d p in c)
+                        {
+                            GL.Vertex3(p.x, p.y, p.z);
+                        }
+                        GL.End();
                     }
-                    GL.End();
-                    GL.LineWidth(LineWidth);
-                    GL.Begin(BeginMode.LineStrip);
-                    foreach (Coord3d p in c)
+                    //Draw lines
+                    if (LineWidth > 0)
                     {
-                        GL.Vertex3(p.x, p.y, p.z);
+                        GL.Begin(BeginMode.LineStrip);
+                        foreach (Coord3d p in c)
+                        {
+                            GL.Vertex3(p.x, p.y, p.z);
+                        }
+                        GL.End();
                     }
-                    GL.End();
                 }
             }
-
-            // doDrawBounds (MISSING)
-
         }
 
         public override Transform.Transform Transform
